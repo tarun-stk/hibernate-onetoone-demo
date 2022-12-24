166243e0ad529436de2caeacb6242fa2c50a21c1
tsoodula <tarun.soodula@capgemini.com>
2022-12-24 16:48:10 +0530

    adding one-to-one-uni file

---
+++ one-to-one-uni.sql
@@ -0,0 +1,28 @@
+use hibernate_mappings; 
+drop table if exists instructor_detail;
+create table instructor_detail(
+	id int auto_increment not null,
+    youtube_profile varchar(128) default null,
+    hobby varchar(45) default null,
+    
+    primary key (id) 
+);
+
+drop table if exists instructor;
+create table instructor(
+	id int auto_increment not null,
+    first_name varchar(45) default null,
+    last_name varchar(45) default null,
+    email varchar(45) default null,
+    instructor_detail_id int default null,
+    
+    key fk_detail_idx (instructor_detail_id),
+    constraint fk_detail foreign key (instructor_detail_id) 
+    references instructor_detail (id) 
+    on delete no action on update no action,
+    primary key (id) 
+);
+
+
+
+
