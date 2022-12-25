70dc4f5d016e6d99c6b5b5ead9442b1606650530
tsoodula <tarun.soodula@capgemini.com>
2022-12-25 11:27:16 +0530

    Added post mapping in instructorController.java

--- hibernate-one-to-one-demo/src/main/java/com/onetoone/mapping/controller/InstructorController.java
+++ hibernate-one-to-one-demo/src/main/java/com/onetoone/mapping/controller/InstructorController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,6 +44,11 @@ public class InstructorController {
 	
 	@PostMapping()
 	public void addInstructor(@RequestBody Instructor theInstructor) {
+		instructorRepositosity.save(theInstructor);
+	}
+	
+	@PutMapping()
+	public void updateInstructor(@RequestBody Instructor theInstructor) {
 		instructorRepositosity.save(theInstructor);
 	}
 	
